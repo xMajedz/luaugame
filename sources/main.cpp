84181e2daa3bf8e37199f7e50bcfa66f377cad19
mntndo <mntndo@protonmail.com>
2024-06-27 08:26:31 +0300

    include more raylib functions

--- sources/main.cpp
+++ sources/main.cpp
@@ -11,6 +11,12 @@ struct Bytecode {
 	char* data;
 };
 
+struct State {
+	bool UpdateFuncExists = true;
+	bool DrawFuncExists = true;
+	bool CustomInitWindow;
+} luaugame_State;
+
 char* read_file(const char* file_name)
 {
 	FILE* f;
@@ -34,7 +40,7 @@ int luaugame_dostring(lua_State* L, const char* string)
 {
 	Bytecode bytecode = {0};
 
-	const char* chunkname = "=luaugame1";
+	const char* chunkname = "=luaugame_dostring";
 
 	bytecode.data = luau_compile(string, strlen(string), NULL, &bytecode.size);
 
@@ -55,7 +61,7 @@ int luaugame_dofile(lua_State* L, const char* file_name)
 	Bytecode bytecode = {0};
 
 	char* source = read_file(file_name);
-	const char* chunkname = "=luaugame2";
+	const char* chunkname = "=luaugame_dofile";
 
 
 	bytecode.data = luau_compile(source, strlen(source), NULL, &bytecode.size);
@@ -74,36 +80,8 @@ int luaugame_dofile(lua_State* L, const char* file_name)
 	return 0;
 }
 
-int luaugame_ClearBackground(lua_State* L)
+int luaugame_setup(lua_State* L)
 {
-	ClearBackground(RED);
-	return 1;
-}
-
-int main()
-{
-	lua_State* L = luaL_newstate();
-
-	lua_pushcfunction(L, luaugame_ClearBackground, "ClearBackground");
-	lua_setglobal(L, "ClearBackground");
-
-	if (luaugame_dostring(L, "luaugame = {}") == LUA_OK) {
-
-	} else {
-
-	}
-
-	if (luaugame_dofile(L, "main.luau") == LUA_OK) {
-
-	} else {
-
-	}
-
-	lua_getglobal(L, "luaugame");
-	if (lua_istable(L, -1)) {
-	} else {
-	}
-
 	lua_pushstring(L, "setup");
 	lua_gettable(L, -2);
 	if (lua_isfunction(L, -1)) {
@@ -111,29 +89,143 @@ int main()
 		} else {
 		}
 	} else {
-		printf("setup not");
+		lua_pop(L, -2);
 	}
 
-	InitWindow(1280, 720, "luaugame");
+	if (!luaugame_State.CustomInitWindow)
+		InitWindow(800, 450, "untitled luaugame");
 
-	while (!WindowShouldClose()) {
-		lua_pushstring(L, "update");
-		lua_gettable(L, -2);
-		if (lua_isfunction(L, -1)) {
-			if (lua_pcall(L, 0, 0, 0) == LUA_OK) {
-			} else {
-			}
+	return 1;
+}
+
+int luaugame_update(lua_State* L)
+{
+	lua_pushstring(L, "update");
+	lua_gettable(L, -2);
+	if (lua_isfunction(L, -1)) {
+		if (lua_pcall(L, 0, 0, 0) == LUA_OK) {
 		} else {
 		}
+	} else {
+		luaugame_State.UpdateFuncExists = false;
+		lua_pop(L, -2);
+	}
+
+	return 1;
+}
+
+int luaugame_draw(lua_State* L)
+{
+	lua_pushstring(L, "draw");
+	lua_gettable(L, -2);
+	if (lua_isfunction(L, -1)) {
+		if (lua_pcall(L, 0, 0, 0) == LUA_OK) {
+		} else {
+		}
+	} else {
+		luaugame_State.DrawFuncExists = false;
+		lua_pop(L, -2);
+	}
+
+	return 1;
+}
+
+int luaugame_InitWindow(lua_State* L)
+{
+	const char* title = lua_tostring(L, -1);
+	int h = lua_tonumber(L, -2);
+	int w = lua_tonumber(L, -3);
+	InitWindow(w, h, title);
+
+	luaugame_State.CustomInitWindow = true;
+
+	return 1;
+}
+
+int luaugame_ClearBackground(lua_State* L)
+{
+	if (!lua_istable(L, -1))
+		return 1;
+
+	unsigned char color[4];
+
+	lua_rawgeti(L, -1, 1);
+	color[0] = lua_tonumber(L, -1);
+	lua_rawgeti(L, -2, 2);
+	color[1] = lua_tonumber(L, -1);
+	lua_rawgeti(L, -3, 3);
+	color[2] = lua_tonumber(L, -1);
+	lua_rawgeti(L, -4, 4);
+	color[3] = lua_tonumber(L, -1);
+
+	ClearBackground((Color){ color[0], color[1], color[2], color[3] });
+
+	return 1;
+}
+
+int luaugame_DrawText(lua_State* L)
+{
+	unsigned char color[4];
+
+	lua_rawgeti(L, -1, 1);
+	color[0] = lua_tonumber(L, -1);
+	lua_rawgeti(L, -2, 2);
+	color[1] = lua_tonumber(L, -1);
+	lua_rawgeti(L, -3, 3);
+	color[2] = lua_tonumber(L, -1);
+	lua_rawgeti(L, -4, 4);
+	color[3] = lua_tonumber(L, -1);
+
+	lua_Number fontSize = lua_tonumber(L, -6);
+	lua_Number y = lua_tonumber(L, -7);
+	lua_Number x = lua_tonumber(L, -8);
+	const char* text = lua_tostring(L, -9);
+
+	DrawText(text, x, y, fontSize, (Color){ color[0], color[1], color[2], color[3] });
+
+	return 1;
+}
+
+int main(int argc, char** argv)
+{
+	lua_State* L = luaL_newstate();
+
+	lua_pushcfunction(L, luaugame_InitWindow, "InitWindow");
+	lua_setglobal(L, "InitWindow");
+
+	lua_pushcfunction(L, luaugame_ClearBackground, "ClearBackground");
+	lua_setglobal(L, "ClearBackground");
+
+	lua_pushcfunction(L, luaugame_DrawText, "DrawText");
+	lua_setglobal(L, "DrawText");
+
+	if (luaugame_dostring(L, "luaugame = {}") == LUA_OK) {
+	} else {
+	}
+
+	if (argc > 1) {
+		if (luaugame_dofile(L, argv[0]) == LUA_OK) {
+		} else {
+		}
+	} else {
+		if (luaugame_dofile(L, "./main.luau") == LUA_OK) {
+		} else {
+		}
+	}
+
+	lua_getglobal(L, "luaugame");
+	if (lua_istable(L, -1)) {
+	} else {
+	}
+	
+	luaugame_setup(L);
+
+	while (!WindowShouldClose()) {
+		if (luaugame_State.UpdateFuncExists)
+			luaugame_update(L);
 		BeginDrawing();
-			lua_pushstring(L, "draw");
-			lua_gettable(L, -2);
-			if (lua_isfunction(L, -1)) {
-				if (lua_pcall(L, 0, 0, 0) == LUA_OK) {
-				} else {
-				}
-			} else {
-			}
+			if (luaugame_State.DrawFuncExists)
+				luaugame_draw(L);
 		EndDrawing();
 	}
 
