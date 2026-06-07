b2c24162a0b7503611ec1de8b9646bc14663e1ec
xMajedz <xmajedz@gmail.com>
2026-06-07 17:22:26 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,14 +1,30 @@
-include "luau_premake5.lua"
-include "raylib_premake5.lua"
+workspace "luaugame"
+    language "C++"
+
+	configurations {"release", "debug"}
+    location "build"
+	targetdir "build"
+	
+	libdirs {"vendor/raylib/src", "vendor/luau/build/release"}
+	includedirs {"vendor/luau/Compiler/include", "vendor/luau/VM/include", "vendor/raylib/src"}
+
+	links {"luaucompiler", "luauast", "luauvm", "luaucommon", "luaucodegen", "raylib"}
+
+	filter { "configurations:release" }
+	defines { "NDEBUG" }
+	optimize "On"
+	
+	filter {"configurations:debug" }
+	defines { "DEBUG" }
+	symbols "On"
 
 project "luaugame"
-	kind "WindowedApp"
-	libdirs {"vendor/raylib/bin/release"}
-	includedirs {"vendor/luau/Compiler/include", "vendor/luau/VM/include", "vendor/raylib/src"}
-	links {"luaucompiler", "luauast", "luauvm", "raylib"}
-	files {"sources/main.cpp"}
-	targetdir "build/${prj.cfg}"
+    kind "WindowedApp"
+    files {"sources/main.cpp"}
 
-    	filter {"system:linux or system:windows", "toolset:mingw"}
-		links {"winmm", "gdi32", "opengl32"}
-		targetextension ".exe"
+	filter {"system:linux or system:windows", "toolset:mingw"}
+    links {"winmm", "gdi32", "opengl32"}
+    targetextension ".exe"
+
+	filter {"system:linux"}
+	links {"X11"}
